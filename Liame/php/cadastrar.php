054a0b3cd223ef560a46ed97827fe016d70ce2d8
Milena Cendrette Ferreira <milenacend04@gmail.com>
2021-08-14 22:46:07 -0300

    foi feito a página de cadastro e de login no php

--- Liame/php/cadastrar.php
+++ Liame/php/cadastrar.php
@@ -1,3 +1,41 @@
 <?php
     echo "página de cadastro";
+
+    $login = $_POST['login'];
+    $senha = MD5 ($_POST['senha']);
+    $connect = mysql_connect('localhost', 'usuario', 'senha');
+    $db = mysql_select_db('liame');
+    $query_select = "SELECT email_mae FROM mae WHERE email_mae = '$login';
+    $select = mysql_query($query_select, $connect);
+    $array = $array['login'];
+
+        if ($login == "" || $login == null){
+            echo "<script language='javascript' type='text/javascript'>
+            alert('O campo login deve ser preenchido');window.location.href='
+            cadastrar.php';</script>";
+
+        }else{
+            if($logarray == $login){
+
+                echo"<script language='javascript' type='text/javascript'>
+                alert('Esse login já existe');window.location.href='
+                cadastrar.php';</script>";
+                die();
+
+        }else{
+            $query = "INSERT INTO mae (email_mae,senha_mae) VALUES ('$login','$senha')";
+            $insert = mysql_query($query,$connect);
+        
+             if($insert){
+                 echo"<script language='javascript' type='text/javascript'>
+                  alert('Usuário cadastrado com sucesso!');window.location.
+                  href='entrar.php'</script>";
+            }else{
+                echo"<script language='javascript' type='text/javascript'>
+                  alert('Não foi possível cadastrar esse usuário');window.location
+                  .href='entrar.php'</script>";
+            }
+        }
+     }
+
 ?>
--- Liame/php/entrar.php
+++ Liame/php/entrar.php
@@ -1,3 +1,25 @@
 <?php
-    echo "página de entar na conta";
+    echo "página de entrar na conta";
+
+   
+$login = $_POST['login'];
+$entrar = $_POST['entrar'];
+$senha = MD5($_POST['senha']);
+$connect = mysql_connect('localhost','nome_de_usuario','senha');
+$db = mysql_select_db('liame');
+  if (isset($entrar)) {
+
+    $verifica = mysql_query("SELECT * FROM mae WHERE email_mae =
+    '$login' AND senha_mae = '$senha'") or die("erro ao selecionar");
+      if (mysql_num_rows($verifica)<=0){
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='entrar.php';</script>";
+        die();
+      }else{
+        setcookie("login",$login);
+        header("Location:index.html");
+      }
+  }
+
 ?>
