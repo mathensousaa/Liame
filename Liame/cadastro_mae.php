b126cbcbf7343c0b987096bbdc4844bd78b4d7bc
Milena Cendrette Ferreira <milenacend04@gmail.com>
2021-08-29 20:11:06 -0300

    cadastro

--- Liame/cadastro_mae.php
+++ Liame/cadastro_mae.php
@@ -2,57 +2,32 @@
 <?php
     include 'php/conexao.php';
 
+    if(isset($_POST['submit'])){
 
-    $nome_mae = $_POST["nome_mae"];
-    $sobrenome_mae = ($_POST["sobrenome_mae"]);
+    $nome_mae = ($_POST['nome_mae']);
+    $sobrenome_mae = ($_POST['sobrenome_mae']);
     $nome_usuario = ($_POST['nome_usuario']);
     $foto_perfil = ($_POST['foto_perfil_mae']);
     $email_mae = ($_POST['email_mae']);
     $senha_mae = ($_POST['senha_mae']);
     $confirmarsenha_mae = ($_POST['confirmarsenha_mae']);
+    
+    
 
-    $array = $array['login'];
+    if($confirmarsenha_mae == $senha_mae){
+      echo "Senhas iguais";
+     }else{
+         echo "Senhas diferentes";
+     }
 
-    if (isset($_POST['enviar'])){
+  $query = "INSERT INTO 'mae' ( 'nome_mae' , 'sobrenome_mae' , 'apelido_mae' , 'email_mae' , 'senha_mae' , 'foto_perfil_mae') VALUES ('$nome_mae', '$sobrenome_mae', '$apelido_mae', '$email_mae', '$senha_mae', '$foto_perfil_mae')";
+  mysqli_query($link,$query) or die("Erro ao cadastrar");
+  
+  echo "Seu cadastro foi realizado com sucesso!<br>Agradecemos a atenção.";
+}else{
+  echo "formulario nao preenchido";
+}
 
-        $tipo=$_POST{['tipo']};
-
-            if ($tipo == 'profissional'){
-
-                if ($login == ' ' || $login == null){
-
-                    echo "O campo login deve ser preenchido";
-
-
-                }else if($logarray == $login){
-
-
-                    echo "Esse login já existe";
-                    die();
-
-                }else{
-                    echo "Usuário cadastrado com sucesso!";
-                    }
-
-            }else{
-
-                    if ($login == '' || $login == null){
-
-                         echo "O campo login deve ser preenchido";
-
-
-                    }else if($logarray == $login){
-
-                         echo "Esse login já existe";
-                         die();
-
-                    }else{
-
-                        echo "Usuário cadastrado com sucesso!";
-                        }
-                      }
-
-    }
 
     ?>
 
