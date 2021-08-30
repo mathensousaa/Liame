af00506ed8f874e407710a7357ab4d7ae69e00a7
Milena Cendrette Ferreira <milenacend04@gmail.com>
2021-08-29 22:26:21 -0300

    cadastro

--- Liame/php/conexao.php
+++ Liame/php/conexao.php
@@ -1,11 +1,17 @@
 <?php
-  $host = "localhost";
-  $user = "root";
-  $pass = "";
-  $db = "Liame";
-  $mysqli = new mysqli($host, $user, $pass, $db);
-
-if($mysqli -> connect_errno)
-    echo "Falha na Conexão: (".$mysqli -> connect_errno.")" .$mysqli ->connect_error;
+  <?php
+  $link = mysqli_connect("localhost", "root", "", "liame");
+   
+  if (!$link) {
+      echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+      echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+      echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+      exit;
+  }
+   
+  echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+   
+ 
+  ?> 
 
 ?>
