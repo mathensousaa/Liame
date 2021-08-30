ce1960c01b2558b881e2d739909a9d3f53fa993e
Matheus de Sousa <88354244+mathensousaa@users.noreply.github.com>
2021-08-29 22:31:07 -0300

    cadastro mae

--- Liame/php/conexao.php
+++ Liame/php/conexao.php
@@ -1,28 +1,14 @@
 <?php
-<<<<<<< HEAD
-  $host = "localhost";
-  $user = "root";
-  $pass = "";
-  $db = "liame";
-  $mysqli = new mysqli($host, $user, $pass, $db);
-
-if($mysqli -> connect_errno)
-    echo "Falha na Conexão: (".$mysqli -> connect_errno.")" .$mysqli ->connect_error;
-=======
-  <?php
   $link = mysqli_connect("localhost", "root", "", "liame");
-   
+
   if (!$link) {
       echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
       echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
       echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
       exit;
   }
-   
-  echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
-   
- 
-  ?> 
->>>>>>> af00506ed8f874e407710a7357ab4d7ae69e00a7
 
-?>
+  echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+
+
+  ?>
