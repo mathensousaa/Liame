b96616d8bcc39d456d00d3791a944ae40ff23e40
Matheus de Sousa <88354244+mathensousaa@users.noreply.github.com>
2021-09-12 22:23:52 -0300

    contato, privacidade e cookies, termos de uso profissional, quem somos, contato

--- Liame/php/contato.php
+++ Liame/php/contato.php
@@ -1,3 +1,121 @@
-<?php
-    echo "página de contato";
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Contato</title>
+
+  <!--implementação bootstrap-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+  <!--css-->
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/termos_uso_profissional.css">
+
+  <!--favicon-->
+  <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-png">
+
+  <!--unicons (icones que serão usados no site)-->
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</head>
+
+<body>
+  <header class="cabecalho">
+    <div class="container-fluid" id="nav-container">
+      <nav class="navbar navbar-expand-lg navbar-light flex-md-row bd-navbar">
+
+        <!--logo-->
+        <a href="index.html" class="navbar-brand ml-5">
+          <img class="logo" src="assets\img\logo-liame-branca.png" alt="Liame">
+        </a>
+        <!--botão para menu hamburguer mobile-->
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target-"#linksnavbar" aria-controls="liksnavbar" aria-expanded="false" aria-label="toggle">
+          <span class="uil uil-bars"></span>
+        </button>
+        <!--link cabeçalho-->
+        <div class="collapse navbar-collapse" id="linksnavbar">
+          <div class="navbar-nav navbar-collapse justify-content-center">
+            <a class="nav-item nav-link" id="especialistas-menu" href="php/especialistas.php">Especialistas</a>
+            <a class="nav-item nav-link" id="consultas-menu" href="php/consultas.php">Consultas</a>
+            <a class="nav-item nav-link" id="diário-de-bordo-menu" href="php/diario-de-bordo.php">Diário de Bordo</a>
+            <a class="nav-item nav-link" id="quem-somos-menu" href="php/especialistas.php">Quem Somos</a>
+            <a class="nav-item nav-link" id="planos-menu" href="php/planos.php">Planos para Especialistas</a>
+          </div>
+          <!--entrar/cadastro-->
+          <div id="login" class="nav navbar-nav mr-5">
+            <div class="nav-item">
+              <a class="nav-item nav-link" href="layouts/entrar.html">
+                <i class="uil uil-user"></i>
+                Entrar
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <div class="container content shadow-lg mt-5 mb-5">
+      <h4 class="mb-4">Contato</h4>
+
+      <p> Nos escontre no Instagram @projeto_liame</p>
+
+        <p> Temos também uma página do facebook Liame, unindo do inicio ao fim</p>
+
+        <p> Nosso e-mail é liametcc2021@gmail.com</p>
+
+
+            <br><br>
+
+  </main>
+
+  <footer class="footer mt-5">
+    <div class="container conteudo-footer">
+      <div class="row">
+        <div class="#">
+          <a href="index.html"><img src="assets/img/logo.png" class="img-fluid logo-footer" alt="logo liame"></a>
+        </div>
+        <div class="col">
+          <h5 class="text-uppercase">Mães</h5>
+          <ul class="list-unstyled">
+            <li class=""><a href="php/marcar_consultas.php">Agendar consulta</a></li>
+            <li class=""><a href="php/especialistas.php">Especialistas</a></li>
+            <li class=""><a href="php/diario_bordo_gestante.php">Diário de bordo</a></li>
+            <li class=""><a href="php/carteirinha_vacinacao.php">Carteira de vacinação</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h5 class="text-uppercase">Especialistas</h5>
+          <ul class="list-unstyled">
+            <li class=""><a href="php/planos.php">Planos para especialistas</a></li>
+            <li class=""><a href="php/central_ajuda.php">Central de ajuda</a></li>
+            <li class=""><a href="termos_uso_profissional.html">Termos de uso de profissional</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h5 class="text-uppercase">Serviços</h5>
+          <ul class="list-unstyled">
+            <li class=""><a href="php/quem_somos.php">Quem somos</a></li>
+            <li class=""><a href="php/contato.php">Contato</a></li>
+            <li class=""><a href="php/privacidade_cookie.php">Privacidade e cookies</a></li>
+            <li class=""><a href="termos_condicoes_uso.html">Termos e condições de uso</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <div class="social-links">
+            <a class="nav-item nav-link" href="https://www.instagram.com/projeto_liame/">
+              <i class="uil uil-instagram"></i>
+            </a>
+            <a class="nav-item nav-link" href="https://www.facebook.com/Liame-unindo-do-início-ao-fim-103264221864132https://www.facebook.com/Liame-unindo-do-início-ao-fim-103264221864132">
+              <i class="uil uil-facebook"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
