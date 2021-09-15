97c1b8e3ea392aa3c5328944c203862f9cc649b9
mathensousaa <mhenrique.sousa43@gmail.com>
2021-09-15 11:38:01 -0300

    cadastro

--- Liame/php/conexao.php
+++ Liame/php/conexao.php
@@ -1,8 +1,6 @@
 <?php
-$link = mysqli_connect("localhost:3306", "root", "", "Liame");
+$link = mysqli_connect("localhost:3307", "root", "", "Liame");
 
-  /*$link = mysqli_connect("localhost: 66", "root", "", "liame");*/
-  /*$link = mysqli_connect("localhost", "root", "", "liame");*/
   if (!$link) {
       echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
       echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
