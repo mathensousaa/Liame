0361adb69687f685f5b3dcd262b2c89e8d633f0e
Milena Soares <80491740+mlnsoares@users.noreply.github.com>
2021-10-04 18:24:38 -0300

    banco atualizado 04/10
    banco atualizado 04/10

--- Liame/liame.sql
+++ Liame/liame.sql
@@ -2,9 +2,9 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Tempo de geração: 22-Set-2021 às 13:53
--- Versão do servidor: 10.4.13-MariaDB
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 04-Out-2021 às 21:22
+-- Versão do servidor: 5.7.31
 -- versão do PHP: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -123,10 +123,24 @@ CREATE TABLE IF NOT EXISTS `doencas_profissional` (
   `id_doencas` int(11) NOT NULL AUTO_INCREMENT,
   `doencas_tratadas` varchar(30) NOT NULL,
   `id_profissional` int(11) DEFAULT NULL,
-  `id_formacao` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_doencas`),
-  KEY `fk_DoencasProfissional` (`id_profissional`),
-  KEY `fk_DoencasFormacao` (`id_formacao`)
+  KEY `fk_DoencasProfissional` (`id_profissional`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+CREATE TABLE IF NOT EXISTS `endereco` (
+  `cep` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_logradouro` varchar(10) NOT NULL,
+  `logradouro` varchar(100) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`cep`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -140,9 +154,7 @@ CREATE TABLE IF NOT EXISTS `especialidade_profissional` (
   `id_especialidade` int(11) NOT NULL AUTO_INCREMENT,
   `especialidade_profissional` varchar(30) NOT NULL,
   `id_profissional` int(11) DEFAULT NULL,
-  `id_formacao` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_especialidade`),
-  KEY `fk_EspecialidadeFormacao` (`id_formacao`),
   KEY `fk_EspecialidadeProfissional` (`id_profissional`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -171,13 +183,9 @@ DROP TABLE IF EXISTS `formacao_profissional`;
 CREATE TABLE IF NOT EXISTS `formacao_profissional` (
   `id_formacao` int(11) NOT NULL AUTO_INCREMENT,
   `formacao` varchar(40) NOT NULL,
-  `id_especialidade` int(11) DEFAULT NULL,
   `id_profissional` int(11) DEFAULT NULL,
-  `id_doencas` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_formacao`),
-  KEY `fk_FormacaoProfissional` (`id_profissional`),
-  KEY `fk_FormacaoEspecialidade` (`id_especialidade`),
-  KEY `fk_FormacaoDoencas` (`id_doencas`)
+  KEY `fk_FormacaoProfissional` (`id_profissional`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -192,14 +200,23 @@ CREATE TABLE IF NOT EXISTS `mae` (
   `nome_mae` varchar(40) NOT NULL,
   `apelido_mae` varchar(40) DEFAULT NULL,
   `email_mae` varchar(100) NOT NULL,
-  `senha_mae` varchar(32) NOT NULL,
-  `foto_perfil_mae` varchar(32) DEFAULT NULL,
+  `senha_mae` varchar(100) NOT NULL,
+  `foto_perfil_mae` varchar(100) DEFAULT NULL,
   `id_bebe` int(11) DEFAULT NULL,
   `id_diario` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_mae`),
   KEY `fk_MaeBebe` (`id_bebe`),
   KEY `fk_MaeDiario` (`id_diario`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `mae`
+--
+
+INSERT INTO `mae` (`id_mae`, `nome_mae`, `apelido_mae`, `email_mae`, `senha_mae`, `foto_perfil_mae`, `id_bebe`, `id_diario`) VALUES
+(1, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', '', NULL, NULL),
+(2, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', '', NULL, NULL),
+(3, 'Milena Soares', 'mlnsoares', 'milena@gmail.com', '25d55ad283aa400af464c76d713c07ad', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -245,24 +262,21 @@ CREATE TABLE IF NOT EXISTS `profissional` (
   `id_profissional` int(11) NOT NULL AUTO_INCREMENT,
   `nome_profissional` varchar(40) NOT NULL,
   `email_profissional` varchar(100) NOT NULL,
-  `senha_profissional` varchar(32) NOT NULL,
-  `foto_perfil_profissional` varchar(32) DEFAULT NULL,
+  `senha_profissional` varchar(100) NOT NULL,
+  `foto_perfil_profissional` varchar(100) DEFAULT NULL,
   `numero_registro_profissional` varchar(20) NOT NULL,
-  `logradouro_profissional` varchar(100) NOT NULL,
   `numero_endereco_profissional` int(10) NOT NULL,
-  `bairro_profissional` varchar(50) NOT NULL,
-  `cidade_profissional` varchar(50) NOT NULL,
-  `estado_profissional` varchar(50) NOT NULL,
+  `descricao_endereco_profissional` varchar(50) NOT NULL,
   `cep_profissional` int(8) NOT NULL,
-  `imagens_profissional` varchar(32) DEFAULT NULL,
+  `imagens_profissional` varchar(100) DEFAULT NULL,
   `telefone_profissional` int(20) DEFAULT NULL,
   `instagram_profissional` varchar(2083) DEFAULT NULL,
   `whatsapp_profissional` varchar(2083) DEFAULT NULL,
   `telegram_profissional` varchar(2083) DEFAULT NULL,
-  `facebook_profissional` varchar(200) DEFAULT NULL,
-  `youtube_profissional` varchar(11) DEFAULT NULL,
+  `facebook_profissional` varchar(2083) DEFAULT NULL,
+  `youtube_profissional` varchar(2083) DEFAULT NULL,
   `linkdin_profissional` varchar(2083) DEFAULT NULL,
-  `sobre_mim_profissional` varchar(250) DEFAULT NULL,
+  `sobre_mim_profissional` varchar(500) DEFAULT NULL,
   `status_profissional` int(1) NOT NULL,
   `id_experiencia` int(11) DEFAULT NULL,
   `id_formacao` int(11) DEFAULT NULL,
@@ -274,7 +288,22 @@ CREATE TABLE IF NOT EXISTS `profissional` (
   KEY `fk_ProfissionalFormacao` (`id_formacao`),
   KEY `fk_ProfissionalDoencas` (`id_doencas`),
   KEY `fk_ProfissionalEspecialidade` (`id_especialidade`),
-  KEY `fk_ProfissionalPlano` (`id_plano_profissional`)
+  KEY `fk_ProfissionalPlano` (`id_plano_profissional`),
+  KEY `fk_ProfissionalEndereco` (`cep_profissional`),
+  KEY `fk_ProfissionalSatus` (`status_profissional`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `status_profissional`
+--
+
+DROP TABLE IF EXISTS `status_profissional`;
+CREATE TABLE IF NOT EXISTS `status_profissional` (
+  `status_profissional` int(1) NOT NULL,
+  `id_profissional` int(11) NOT NULL,
+  KEY `fk_StatusProfissional` (`id_profissional`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
