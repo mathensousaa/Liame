9840a58b402a317e88595e6927a5c6ebcf082cd3
mlnsoares <soares.milena2003@gmail.com>
2021-10-24 22:42:52 -0300

    cadastro profissional por sessao

--- Liame/php/cadastro_profissional1.php
+++ Liame/php/cadastro_profissional1.php
@@ -1,5 +1,7 @@
 <form action="cadastro_profissional2.php" method="post"/>
     Nome:<input name="nome_profissional" type="text"/></br>
-    Foto de perfil:<input name="foto_perfil_profissional" type="text"/></br>
+    Especialidade:<input name="especialidade_profissional" type="text"/></br>
+    Número de registro:<input name="numero_registro_profissional" type="text"/></br>
+
     <input type="submit" value="Avançar"/>
 </form>
--- Liame/php/cadastro_profissional2.php
+++ Liame/php/cadastro_profissional2.php
@@ -1,53 +1,104 @@
 <?php
     session_start();
     $_SESSION['nome_profissional'] = $_POST["nome_profissional"];
-    $_SESSION['foto_perfil_profisisonal'] = $_POST["foto_perfil_profissional"];
+    $_SESSION['especialidade_profisisonal'] = $_POST["especialidade_profisisonal"];
+    $_SESSION['numero_registro_profisisonal'] = $_POST["numero_registro_profisisonal"];
 ?>
 
 <form action="cadastro_profissional3.php" method="post"/>
-    Telefone:<input name="telefone_profissional" type="text"/></br>
 
     CEP:<input name="cep_profissional" type="text"/></br>
 
-    Tipo de logradouro:<input name="tipo_logradouro_profissional" type="radio" value="aeroporto"/>Aeroporto
-    <input name="tipo_logradouro_profissional" type="radio" value="alameda"/>Alameda
-    <input name="tipo_logradouro_profissional" type="radio" value="area"/>Área
-    <input name="tipo_logradouro_profissional" type="radio" value="avenida"/>Avenida
-    <input name="tipo_logradouro_profissional" type="radio" value="campo"/>Campo
-    <input name="tipo_logradouro_profissional" type="radio" value="chacara"/>Chácara
-    <input name="tipo_logradouro_profissional" type="radio" value="colonia"/>Colônia
-    <input name="tipo_logradouro_profissional" type="radio" value="condominio"/>Condomínio
-    <input name="tipo_logradouro_profissional" type="radio" value="conjunto"/>Conjunto
-    <input name="tipo_logradouro_profissional" type="radio" value="distrito"/>Distrito
-    <input name="tipo_logradouro_profissional" type="radio" value="esplanada"/>Esplanada
-    <input name="tipo_logradouro_profissional" type="radio" value="estacao"/>Estação
-    <input name="tipo_logradouro_profissional" type="radio" value="estrada"/>Estrada
-    <input name="tipo_logradouro_profissional" type="radio" value="favela"/>Favela
-    <input name="tipo_logradouro_profissional" type="radio" value="fazenda"/>Fazenda
-    <input name="tipo_logradouro_profissional" type="radio" value="feira"/>Feira
-    <input name="tipo_logradouro_profissional" type="radio" value="jardim"/>Jardim
-    https://www.significados.com.br/logradouro/
-    <input name="tipo_logradouro_profissional" type="radio" value="alameda"/>Alameda
-    <input name="tipo_logradouro_profissional" type="radio" value="area"/>Área
-    <input name="tipo_logradouro_profissional" type="radio" value="avenida"/>Avenida
-    <input name="tipo_logradouro_profissional" type="radio" value="campo"/>Campo
-    <input name="tipo_logradouro_profissional" type="radio" value="chacara"/>Chácara
-    <input name="tipo_logradouro_profissional" type="radio" value="colonia"/>Colônia
-    <input name="tipo_logradouro_profissional" type="radio" value="condominio"/>Condomínio
-    <input name="tipo_logradouro_profissional" type="radio" value="conjunto"/>Conjunto
-    <input name="tipo_logradouro_profissional" type="radio" value="distrito"/>Distrito
-    <input name="tipo_logradouro_profissional" type="radio" value="esplanada"/>Esplanada
-    <input name="tipo_logradouro_profissional" type="radio" value="estacao"/>Estação
-    <input name="tipo_logradouro_profissional" type="radio" value="estrada"/>Estrada
-    <input name="tipo_logradouro_profissional" type="radio" value="favela"/>Favela
-    <input name="tipo_logradouro_profissional" type="radio" value="fazenda"/>Fazenda
-    <input name="tipo_logradouro_profissional" type="radio" value="feira"/>Feira
-    <input name="tipo_logradouro_profissional" type="radio" value="jardim"/>Jardim
-    
+    <label>Tipo de logradouro:</label>
+    <select>
+        <option selected disabled value="">Selecione</option>
+        <option>Aeroporto</option>
+        <option>Alameda</option>
+        <option>Área</option> 
+        <option>Avenida</option>
+        <option>Campo</option>
+        <option>Chácara</option>
+        <option>Colônia</option>
+        <option>Condomínio</option>
+        <option>Conjunto</option>
+        <option>Distrito</option>
+        <option>Esplanada</option> 
+        <option>Estação</option>
+        <option>Estrada</option>
+        <option>Favela</option>
+        <option>Fazenda</option>
+        <option>Feira</option> 
+        <option>Jardim</option>
+        <option>Ladeira</option>
+        <option>Lago</option>
+        <option>Lagoa</option>
+        <option>Largo</option>
+        <option>Loteamento</option>
+        <option>Morro</option>
+        <option>Núcleo</option> 
+        <option>Parque</option>
+        <option>Passarela</option>
+        <option>Pátio</option>
+        <option>Praça</option>
+        <option>Quadra</option>
+        <option>Recanto</option>
+        <option>Ricidencial</option>
+        <option>Rodovia</option> 
+        <option>Rua</option>
+        <option>Setor</option>
+        <option>Sítio</option>
+        <option>Travessa</option>
+        <option>Trecho</option>
+        <option>Trevo</option>
+        <option>Vale</option>
+        <option>Vereda</option> 
+        <option>Via</option>
+        <option>Viaduto</option>
+        <option>Viela</option>
+        <option>Vila</option> 
+    </select></br>
+
     Nome do logradouro:<input name="nome_logradouro_profissional" type="text"/></br>
+
     Número:<input name="numero_endereco_profissional" type="text"/></br>
+
     Bairro:<input name="bairro_profissional" type="text"/></br>
+
     Cidade:<input name="cidade_profissional" type="text"/></br>
-    UF:<input name="UF_profissional" type="text"/></br>
+
+    <label>UF:</label>
+    <select>
+        <option selected disabled value="">Selecione</option>
+        <option>AC</option>
+        <option> AL</option>
+        <option>AP</option> 
+        <option>AM</option>
+        <option>BA</option>
+        <option>CE</option>
+        <option>DF</option>
+        <option>ES</option>
+        <option>GO</option>
+        <option>MA</option>
+        <option>MG</option> 
+        <option>MS</option>
+        <option>MT</option>
+        <option>PA</option>
+        <option>PB</option>
+        <option>PE</option> 
+        <option>PI</option>
+        <option>PR</option>
+        <option>RJ</option>
+        <option>RN</option>
+        <option>RS</option>
+        <option>RO</option>
+        <option>RR</option>
+        <option>SC</option> 
+        <option>SP</option>
+        <option>SE</option>
+        <option>TO</option>
+    </select></br>
+
+    Complemento: <input name="complemento_endereco_profissional" type="text"/></br>
+
     <input type="submit" value="Avançar"/>
 </form>
