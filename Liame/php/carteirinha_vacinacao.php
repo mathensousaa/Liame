22ff86750fbb81a50485e1c233b0e9484dc7be4f
mlnsoares <soares.milena2003@gmail.com>
2021-10-28 23:40:49 -0300

    cadastro mae

--- Liame/php/carteirinha_vacinacao.php
+++ Liame/php/carteirinha_vacinacao.php
@@ -1,19 +1,10 @@
 <?php
-    session_start();
-    
-    if((!isset ($_SESSION['email_mae']) == true) and (!isset ($_SESSION['nome_mae']) == true)){
-        
-        unset($_SESSION['email_mae']);
-        unset($_SESSION['nome_mae']);
-
-        echo "Bem vindo $logado";
-        
+  session_start(); 
+  if((!isset ($_SESSION['email_mae']) == true) and (!isset ($_SESSION['nome_mae']) == true)){ 
+    unset($_SESSION['email_mae']);
+    unset($_SESSION['nome_mae']);
+    echo "Bem vindo $logado";    
   } 
-
   echo "uauuu";
- 
-
-$logado = $_SESSION['nome_mae'];
+  $logado = $_SESSION['nome_mae'];
 ?>
-
-
