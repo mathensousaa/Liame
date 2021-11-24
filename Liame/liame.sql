fa243dd111bcdcaeaf0c9dacca1e052f34521764
Milena Soares <80491740+mlnsoares@users.noreply.github.com>
2021-11-24 11:06:47 -0300

    banco de dados 24/11

--- Liame/liame.sql
+++ Liame/liame.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Tempo de geração: 16-Nov-2021 às 18:05
+-- Tempo de geração: 24-Nov-2021 às 14:05
 -- Versão do servidor: 10.4.13-MariaDB
 -- versão do PHP: 7.3.21
 
