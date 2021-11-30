25d8e13fc214646662a6cb94a148467bd46ce4ec
Ana Lopes <flavinha21.mell@gmail.com>
2021-11-30 19:55:44 -0300

    teste2

--- Liame/php/carteirinha_vacinacao.php
+++ Liame/php/carteirinha_vacinacao.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>Carteirinha de vacinação | Liame</title>
+  <title>Central de ajuda | Liame</title>
 
   <!-- CSS -->
   <link rel="stylesheet" href="../assets/css/owl/owl.carousel.min.css">
@@ -48,14 +48,6 @@
                   <a class="nav-link" href="../php/diario_bordo.php">Diário de Bordo</a>
                 </li>
               </ul>
-              <!--entrar/cadastro-->
-              <div id="login" class="nav navbar-nav">
-                <div class="nav-item">
-                  <button type="button" class="button button-primary btn btn-outline-primarynav-item " data-bs-toggle="modal" data-bs-target="#modal-login">
-                    Entrar
-                  </button>
-                  </a>
-
 
 <?php
   /*session_start();
@@ -86,18 +78,18 @@
 
       }
     }
-  }*/
-  //start session
-  if (!isset($_SESSION)) {//Verificar se a sessão não já está aberta.
-    session_start();
-  }
+      }*/
+      //start session
+      if (!isset($_SESSION)) {//Verificar se a sessão não já está aberta.
+        session_start();
+      }
 
-//check do the person logged in
-if($_SESSION['id_mae']==NULL){
-    //haven't log in
+    //check do the person logged in
+    if($_SESSION['id_mae']==NULL){
+        //haven't log in
 
-}else{
-  ?>
+    }else{
+      ?>
 
   <body>
     <div id="cartilha" class="container-fluid block position-relative">
@@ -485,36 +477,35 @@ if($_SESSION['id_mae']==NULL){
         </div>
       </div>
     </div>
-
     <footer>
     <div id="rodape" class="container">
       <div class="conteudo py-5">
         <div class="row">
           <div class="logo col-md-2">
-            <img id="logo" class="img-fluid" src="../assets/img/logo_liame.png" alt="">
+            <img id="logo" class="img-fluid" src="assets/img/logo_liame.png" alt="">
           </div>
           <div class="py-lg-0 py-mb-5 col-md-8 d-flex justify-content-center align-self-center">
             <div class="px-lg-4 px-mb-1">
               <h5 class="text-uppercase">Mães</h5>
               <ul class="list-unstyled">
-                <li class=""><a href="../php/procura_profissional1.php">Consultas</a></li>
-                <li class=""><a href="../php/diario_bordo.php">Diário de bordo</a></li>
-                <li class=""><a href="../php/cadastro_mae1.php">Carteira de vacinação</a></li>
+                <li class=""><a href="php/procura_profissional1.php">Consultas</a></li>
+                <li class=""><a href="php/diario_bordo.php">Diário de bordo</a></li>
+                <li class=""><a href="php/carteirinha_vacinacao.php">Carteira de vacinação</a></li>
               </ul>
             </div>
             <div class="px-lg-4 px-mb-1">
               <h5 class="text-uppercase">Especialistas</h5>
               <ul class="list-unstyled">
-                <li class=""><a href="central_ajuda.html">Central de ajuda</a></li>
-                <li class=""><a href="termos_uso_profissional.html">Termos de uso de profissional</a></li>
+                <li class=""><a href="layouts/central_ajuda.html">Central de ajuda</a></li>
+                <li class=""><a href="layouts/termos_uso_profissional.html">Termos de uso de profissional</a></li>
               </ul>
             </div>
             <div class="px-lg-4 px-mb-1">
               <h5 class="text-uppercase">Serviços</h5>
               <ul class="list-unstyled">
-                <li class="list-item"><a href="quem_somos.html">Quem somos</a></li>
-                <li class=""><a href="Contatos.html">Contato</a></li>
-                <li class=""><a href="termos_condicoes_uso.html">Termos e condições de uso</a></li>
+                <li class="list-item"><a href="layouts/quem_somos.html">Quem somos</a></li>
+                <li class=""><a href="layouts/Contatos.html">Contato</a></li>
+                <li class=""><a href="layouts/termos_condicoes_uso.html">Termos e condições de uso</a></li>
               </ul>
             </div>
           </div>
@@ -522,12 +513,12 @@ if($_SESSION['id_mae']==NULL){
             <ul>
               <li>
                 <a href="https://www.instagram.com/projeto_liame/">
-                <img src="../assets/img/instagram.svg" alt="Icone Instagram">
+                <img src="assets/img/instagram.svg" alt="Icone Instagram">
                 </a>
               </li>
               <li>
                 <a href="https://www.facebook.com/Liame-unindo-do-início-ao-fim-103264221864132">
-                <img src="../assets/img/facebook.svg" alt="Icone Facebook">
+                <img src="assets/img/facebook.svg" alt="Icone Facebook">
                 </a>
               </li>
             </ul>
@@ -536,13 +527,18 @@ if($_SESSION['id_mae']==NULL){
       </div>
     </div>
   </footer>
-    <!-- //Cartilha -->
-    <!--implementação jquery, poppers.js e plugin bootstrap-->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="../assets/js/owl.carousel.min.js"></script>
-    <script src="../assets/js/main.js"></script>
+
+
+  <!--implementação jquery, poppers.js e plugin bootstrap-->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js" integrity="sha512-yDlE7vpGDP7o2eftkCiPZ+yuUyEcaBwoJoIhdXv71KZWugFqEphIS3PU60lEkFaz8RxaVsMpSvQxMBaKVwA5xg==" crossorigin="anonymous"
+    referrerpolicy="no-referrer"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <script src="assets/js/main.js"></script>
+
+
   <?php
 }
   ?>
