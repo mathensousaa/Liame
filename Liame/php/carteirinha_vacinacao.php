61ac7a57c2313ca09dd07aa1620a7c486a45d653
Ana Lopes <flavinha21.mell@gmail.com>
2021-11-30 20:06:18 -0300

    carteirinha de vacinação está de volta

--- Liame/php/carteirinha_vacinacao.php
+++ Liame/php/carteirinha_vacinacao.php
@@ -5,49 +5,17 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <title>Central de ajuda | Liame</title>
-
+  <title>Cartilha de vacinação</title>
   <!-- CSS -->
   <link rel="stylesheet" href="../assets/css/owl/owl.carousel.min.css">
   <link rel="stylesheet" href="../assets/css/owl/owl.theme.default.min.css">
   <link rel="stylesheet" href="../assets/css/main.css">
-
   <!--favicon-->
   <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-png">
-
   <!--unicons (icones que serão usados no site)-->
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-
 </head>
 
-<body>
-  <main>
-    <!--navbar-->
-    <div id="header" class="fixed-top">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
-          <div class="container-fluid">
-            <a href="../index.php" class="navbar-brand">
-              <img class="img-fluid" id="logo" src="../assets/img/logo_liame.png" alt="Liame">
-            </a>
-
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" href="../index.php">Página Inicial</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="../php/procura_profissional1.php">Buscar especialistas</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="../php/diario_bordo.php">Diário de Bordo</a>
-                </li>
-              </ul>
 
 <?php
   /*session_start();
@@ -78,18 +46,18 @@
 
       }
     }
-      }*/
-      //start session
-      if (!isset($_SESSION)) {//Verificar se a sessão não já está aberta.
-        session_start();
-      }
+  }*/
+  //start session
+  if (!isset($_SESSION)) {//Verificar se a sessão não já está aberta.
+    session_start();
+  }
 
-    //check do the person logged in
-    if($_SESSION['id_mae']==NULL){
-        //haven't log in
+//check do the person logged in
+if($_SESSION['id_mae']==NULL){
+    //haven't log in
 
-    }else{
-      ?>
+}else{
+  ?>
 
   <body>
     <div id="cartilha" class="container-fluid block position-relative">
@@ -477,68 +445,13 @@
         </div>
       </div>
     </div>
-    <footer>
-    <div id="rodape" class="container">
-      <div class="conteudo py-5">
-        <div class="row">
-          <div class="logo col-md-2">
-            <img id="logo" class="img-fluid" src="assets/img/logo_liame.png" alt="">
-          </div>
-          <div class="py-lg-0 py-mb-5 col-md-8 d-flex justify-content-center align-self-center">
-            <div class="px-lg-4 px-mb-1">
-              <h5 class="text-uppercase">Mães</h5>
-              <ul class="list-unstyled">
-                <li class=""><a href="php/procura_profissional1.php">Consultas</a></li>
-                <li class=""><a href="php/diario_bordo.php">Diário de bordo</a></li>
-                <li class=""><a href="php/carteirinha_vacinacao.php">Carteira de vacinação</a></li>
-              </ul>
-            </div>
-            <div class="px-lg-4 px-mb-1">
-              <h5 class="text-uppercase">Especialistas</h5>
-              <ul class="list-unstyled">
-                <li class=""><a href="layouts/central_ajuda.html">Central de ajuda</a></li>
-                <li class=""><a href="layouts/termos_uso_profissional.html">Termos de uso de profissional</a></li>
-              </ul>
-            </div>
-            <div class="px-lg-4 px-mb-1">
-              <h5 class="text-uppercase">Serviços</h5>
-              <ul class="list-unstyled">
-                <li class="list-item"><a href="layouts/quem_somos.html">Quem somos</a></li>
-                <li class=""><a href="layouts/Contatos.html">Contato</a></li>
-                <li class=""><a href="layouts/termos_condicoes_uso.html">Termos e condições de uso</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="social col-md-2">
-            <ul>
-              <li>
-                <a href="https://www.instagram.com/projeto_liame/">
-                <img src="assets/img/instagram.svg" alt="Icone Instagram">
-                </a>
-              </li>
-              <li>
-                <a href="https://www.facebook.com/Liame-unindo-do-início-ao-fim-103264221864132">
-                <img src="assets/img/facebook.svg" alt="Icone Facebook">
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-
-  <!--implementação jquery, poppers.js e plugin bootstrap-->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js" integrity="sha512-yDlE7vpGDP7o2eftkCiPZ+yuUyEcaBwoJoIhdXv71KZWugFqEphIS3PU60lEkFaz8RxaVsMpSvQxMBaKVwA5xg==" crossorigin="anonymous"
-    referrerpolicy="no-referrer"></script>
-  <script src="assets/js/owl.carousel.min.js"></script>
-  <script src="assets/js/main.js"></script>
-
-
+    <!-- //Cartilha -->
+    <!--implementação jquery, poppers.js e plugin bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="../assets/js/owl.carousel.min.js"></script>
+    <script src="../assets/js/main.js"></script>
   <?php
 }
   ?>
