6d4b0e295dd00ca3d488a61f58817df8578e35f5
mlnsoares <soares.milena2003@gmail.com>
2021-11-30 20:09:43 -0300

    banco 30/11

--- Liame/liame.sql
+++ Liame/liame.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Tempo de geração: 30-Nov-2021 às 23:07
+-- Tempo de geração: 30-Nov-2021 às 23:09
 -- Versão do servidor: 10.4.13-MariaDB
 -- versão do PHP: 7.3.21
 
