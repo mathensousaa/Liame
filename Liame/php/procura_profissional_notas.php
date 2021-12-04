4d61ae048cea9f17c99a561d4b6ecfefa42afea5
mlnsoares <soares.milena2003@gmail.com>
2021-12-04 18:01:13 -0300

    procura profissional (exibir perfil)

---
+++ Liame/php/procura_profissional_notas.php
@@ -0,0 +1,8 @@
+<?php
+// ANOTAÇÃO PROCURA PROFISSIONAL SOARES
+// PELO AMOR DE DEUS NÃO APAGA 😭😭🙏🏼
+
+    "red.id_profissional, youtube_profissional, whatsapp_profissional, telegram_profissional, facebook_profissional, linkedin_profissional, instagram_profissional
+    
+    inner join rede_social_profissional as red on red.id_ = pro.id_profissional"
+?>
