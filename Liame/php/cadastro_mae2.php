4e6e3957a17dd818fd30f55030d9d93efe26e772
mlnsoares <soares.milena2003@gmail.com>
2021-12-08 09:57:35 -0300

    cadastro profissional

--- Liame/php/cadastro_mae2.php
+++ Liame/php/cadastro_mae2.php
@@ -36,7 +36,8 @@
             $senha_criptografada_mae = md5($senha_mae);
 
             //inserir no banco de dados
-            $query = 'INSERT INTO mae (nome_mae, email_mae, senha_mae) VALUES ("'.$nome_mae.'", "'.$email_mae.'", "'.$senha_criptografada_mae.'")';
+            $query = 'INSERT INTO mae (nome_mae, email_mae, senha_mae) 
+            VALUES ("'.$nome_mae.'", "'.$email_mae.'", "'.$senha_criptografada_mae.'")';
             
             if (mysqli_query($link, $query)){
                 echo "Cadastrado com sucesso";
@@ -45,18 +46,12 @@
                 echo "Erro ao cadastrar";
             }
         }
-       
-        ?> 
-
-        
+        ?>         
         <?php 
             }else{
                 header('location: recuperacao.php');
-               
-             }
-             ?> 
-             
+            }
+        ?>   
         </div>
-    
     </body>
     </html>
--- Liame/php/cadastro_profissional2.php
+++ Liame/php/cadastro_profissional2.php
@@ -1,20 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Título da página</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../assets/css/owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="../assets/css/owl/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../assets/css/custom.css">
+   
+  </head>
+  <body>
+    
+  
 <?php
     session_start();
     include ('conexao.php');
 
-    if(isset($_POST['submit'])){
-//variavel da sessao
-    $nome_profissional = $_POST['nome_profissional'];
+    //variavel da sessao
+    $nome_profissional = $_POST["nome_profissional"];
     $email_profissional = $_POST['email_profissional'];
     $senha_profissional = $_POST['senha_profissional'];
     $confirmarsenha_profissional = $_POST['confirmarsenha_profissional'];
 
-// validacao da senha
+    // validacao da senha
     if($confirmarsenha_profissional == $senha_profissional){
+
         //checando se o email ja existe
         $user_check_query = "SELECT * FROM profissional WHERE email_profissional = '$email_profissional'";
         $resultado = mysqli_query($link, $user_check_query);
         $conta = mysqli_num_rows($resultado);
+
         if($conta>=1){
             header('location: email_profissional.php');
         }else{
@@ -23,18 +37,23 @@
 
             //inserir no banco de dados
             $query = 'INSERT INTO profissional (nome_profissional, email_profissional, senha_profissional) 
-                      VALUES ("'.$nome_profissional.'", "'.$email_profissional.'", "'.$senha_criptografada_profissional.'")';
-           echo $query;
-           if (mysqli_query($link, $query)){
-                header('Location: perfil_profissional1.php');
+            VALUES ("'.$nome_profissional.'", "'.$email_profissional.'", "'.$senha_criptografada_profissional.'")';
+            
+            echo $query . '<br>';
+
+            if (mysqli_query($link, $query)){
+                echo "Cadastrado com sucesso";
+                header('Location: login_profissional.php');
             }else{
                 echo "Erro ao cadastrar";
             }
         }
-    }else{
-        header('location: recuperacao_profissional.php');
-       
-    }     
-    session_destroy(); 
-}
-?>
+        ?> 
+        <?php 
+            }else{
+                header('location: recuperacao.php');
+            }
+        ?> 
+        </div>
+    </body>
+    </html>
