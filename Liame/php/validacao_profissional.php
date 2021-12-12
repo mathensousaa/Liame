741b4b2c2485f90b1fde8b38bd2452703ccba0d4
mlnsoares <soares.milena2003@gmail.com>
2021-12-12 00:13:04 -0300

    pagina de adm

--- Liame/php/validacao_profissional.php
+++ Liame/php/validacao_profissional.php
@@ -26,18 +26,19 @@
     
     $status = 0;
     $documentacao = "";
-    $sql_busca = 'SELECT nome_profissional, email_profissional, status_validacao_profissional from profissional 
+    $sql_busca = 'SELECT id_profissional, nome_profissional, email_profissional, status_validacao_profissional, 
+                  documentacao_validacao_profissional from profissional 
                   WHERE status_validacao_profissional = "'.$status.'" 
                   AND documentacao_validacao_profissional != "'.$documentacao.'"';
+    
                 $consulta = mysqli_query($link, $sql_busca);
 						
-
                 if($consulta->num_rows > 0){
                   while ($vetor = mysqli_fetch_array($consulta)){
+                    $id_profissional = $vetor['id_profissional'];
                     $nome_profissional= $vetor['nome_profissional'];
-                    //$especialidade_profissional=$vetor['especialidade'];
                     $email_profissional=$vetor['email_profissional'];
-                   // $documentacao_profissional=$vetor['documentacao_validacao_profissional'];
+                    $documentacao_profissional=$vetor['documentacao_validacao_profissional'];
                     $status_profissional=$vetor['status_validacao_profissional'];
 ?>
                         <div class="col-sm-3">
@@ -45,8 +46,12 @@
                             <div class="card-body">
                               <?php
                                 echo utf8_encode ('<b>Nome: </b>' .$nome_profissional.'<br>');
-                                echo utf8_encode ('<b>Especialidade: </b>' .$especialidade_profissional.'<br>');	
-                                echo utf8_encode ('<b>Documentaçao: </b>' .$documentacao_profissional.'<br>');	
+                                echo utf8_encode ('<b>Email: </b>' .$email_profissional.'<br>');
+                                echo utf8_encode ('<b>Documentação: </b>' .$documentacao_profissional.'<br>');
+                                echo "<a href=aprovar_profissional.php?id=".$vetor['id_profissional'].">Aprovar</a><br>";
+                                echo "<a href=reprovar_profissional.php?id=".$vetor['id_profissional'].">Reprovar</a>";
+              
+                                ?><hr><?php	
                               ?>
                             </div>                    
                           </div>
@@ -56,7 +61,9 @@
                   }
               }else{
                 echo "Nenhum profissional para ser avaliado.";
-              }
+              
+  }
+
             ?>
             </div>
           </div>
@@ -66,17 +73,16 @@
             </div>
           </div>
         <?php
-<<<<<<< HEAD
+
   }else if($id_adm == 0){
     header('Location: ../index.php');
       
-    }
-=======
-  }else{
+  }
+
+  else{
     
     header("Location:../index.php");
 
   }
->>>>>>> 52cd332ad0787b50a6551a80eb57e5c1de2668c6
   ?>
   
