95ea7b2b9e53e616ca6991b327255e96615fd72e
mlnsoares <soares.milena2003@gmail.com>
2021-12-12 00:27:35 -0300

    comentario para o back da pagina de adm e procura profissional

--- Liame/php/validacao_profissional.php
+++ Liame/php/validacao_profissional.php
@@ -26,13 +26,15 @@
     
     $status = 0;
     $documentacao = "";
+    //o profissional só é avaliado se ele tiver o status igual a 0 (se for igual a 0 é porque ele não passou por nenhuma verificação ainda) e se ele já tiver adicionado algum documento
     $sql_busca = 'SELECT id_profissional, nome_profissional, email_profissional, status_validacao_profissional, 
                   documentacao_validacao_profissional from profissional 
                   WHERE status_validacao_profissional = "'.$status.'" 
                   AND documentacao_validacao_profissional != "'.$documentacao.'"';
     
                 $consulta = mysqli_query($link, $sql_busca);
-						
+
+		// transformando os dados do banco em variavel
                 if($consulta->num_rows > 0){
                   while ($vetor = mysqli_fetch_array($consulta)){
                     $id_profissional = $vetor['id_profissional'];
@@ -45,9 +47,11 @@
                           <div class="card">
                             <div class="card-body">
                               <?php
+                        //exibindo as variaveis
                                 echo utf8_encode ('<b>Nome: </b>' .$nome_profissional.'<br>');
                                 echo utf8_encode ('<b>Email: </b>' .$email_profissional.'<br>');
                                 echo utf8_encode ('<b>Documentação: </b>' .$documentacao_profissional.'<br>');
+                        // fazendo o link para ele ser aprovado ou reprovado
                                 echo "<a href=aprovar_profissional.php?id=".$vetor['id_profissional'].">Aprovar</a><br>";
                                 echo "<a href=reprovar_profissional.php?id=".$vetor['id_profissional'].">Reprovar</a>";
               
